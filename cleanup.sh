eb79c3ca15134084882e9c7b513e5b0a7d816b2c
Eric Kightley <eric@respond-software.com>
2019-11-21 10:51:43 -0800

    stop removing the wrap.c and .py files

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-rm -r swig_mwe/fastLA/__pycache__/ build/ swig_mwe/fastLA/_fastLA.cpython-37m-darwin.so
-rm -r swig_mwe/fastLA/fastLA.py swig_mwe/fastLA/fastLA_wrap.c swig_mwe/__pycache__ tests/__pycache__
+rm -r build/ swig_mwe/fastLA/_fastLA.cpython-37m-darwin.so
